280e6704949a41b76da389e7d6038642edb519a0
Huy <24520678@gm.uit.edu.vn>
2026-04-16 21:02:57 +0700

    add json convert function and save() function

--- backend/src/main/java/com/attvs/backend/service/KafkaConsumerService.java
+++ backend/src/main/java/com/attvs/backend/service/KafkaConsumerService.java
@@ -1,16 +1,44 @@
 package com.attvs.backend.service;
 
+import com.attvs.backend.entity.AttackEvent;
+import com.attvs.backend.repository.AttackEventRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 public class KafkaConsumerService {
 
-    // Đứng canh ở băng chuyền Kafka tên "attack-events"
+    private final AttackEventRepository repository;
+    private final ObjectMapper objectMapper;
+
+
+    public KafkaConsumerService(AttackEventRepository repository) {
+        this.repository = repository;
+        this.objectMapper = new ObjectMapper();
+        // Để Jackson đọc được kiểu dữ liệu thời gian (Instant, LocalDateTime)
+        this.objectMapper.registerModule(new JavaTimeModule());
+    }
+
     @KafkaListener(topics = "attack-events", groupId = "attack-group")
     public void consumeAttackEvent(String message) {
-        System.out.println("🔥 NI ROI CAC CHAU OI! Kafka attacked: " + message);
+        try {
 
+            AttackEvent event = objectMapper.readValue(message, AttackEvent.class);
 
+            if (event.getId() == null) event.setId(UUID.randomUUID());
+            if (event.getTimestamp() == null) event.setTimestamp(Instant.now());
+
+            repository.save(event)
+                    .doOnSuccess(saved -> System.out.println("DB SAVED: ID=" + saved.getId() + " IP=" + saved.getSrcIp()))
+                    .doOnError(err -> System.err.println("DB ERROR: " + err.getMessage()))
+                    .subscribe(); // IMPORTANT!!!!!!!!!!!!!!!!!!!!!!!!
+
+        } catch (Exception e) {
+            System.err.println("JSON DECODING ERROR: " + e.getMessage());
+        }
     }
 }
